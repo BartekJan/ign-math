fe8a8ff2aed2f2a60df5403bc8c04c99972e6cfc
chapulina <burajiru.no.chapulina@gmail.com>
2017-02-14 08:32:00 -0800

    Merge ign-math2 into ign-math3

--- src/Vector4_TEST.cc
+++ src/Vector4_TEST.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2014 Open Source Robotics Foundation
+ * Copyright (C) 2012 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -141,7 +141,7 @@ TEST(Vector4dTest, Vector4d)
 }
 
 /////////////////////////////////////////////////
-TEST(Vector4dTest, IndexException)
+TEST(Vector4dTest, NoException)
 {
   math::Vector4d v(1, 2, 3, 4);
   EXPECT_NO_THROW(math::equal(v[0], 1.0));
@@ -149,7 +149,8 @@ TEST(Vector4dTest, IndexException)
   EXPECT_NO_THROW(math::equal(v[2], 3.0));
   EXPECT_NO_THROW(math::equal(v[3], 4.0));
 
-  EXPECT_THROW(math::equal(v[4], 5.0), math::IndexException);
+  EXPECT_NO_THROW(math::equal(v[4], 5.0));
+  EXPECT_DOUBLE_EQ(v[4], 4.0);
 }
 
 /////////////////////////////////////////////////
